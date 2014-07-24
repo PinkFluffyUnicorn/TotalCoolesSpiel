2cc9e18113db9ac05885faf009973ff2706506e4
Haschiwu <christoph.hollmann@yahoo.de>
2014-07-24 21:39:25 +0200

    dtt

---
+++ Vampir/Vampir/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Vampir
+{
+    class Player
+    {
+
+        Vector2f _position;
+        Sprite _sprite;
+
+        public Player(string path)
+        {
+            _position = new Vector2f(100, 110);
+            Texture tex = new Texture(path);
+            _sprite = new Sprite(tex);
+        }
+        
+        public void Update() {
+            _position.X +=1;
+            if (_position.X > 700) _position.X = 1;
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            _sprite.Position = _position;
+            window.Draw(_sprite);
+        }
+
+    }
+}
