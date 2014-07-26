7bfabf6671b2b94c4579f850d52cc65174d56ad6
Sara <sara.zecha@unitybox.de>
2014-07-26 18:48:06 +0200

    badum tsss

---
+++ Vampir/Vampir/Const.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+using Vampir;
+
+namespace Vampir
+{
+    class Const
+    {
+        public static float jumpHeight = 150f;
+        public static int jumptime = 700;
+        public static float jumpspeed = 0.7f;
+        public static float groundHeight = 400f;
+        static float moveSpeed = 1f;
+        public static float monsterSpeedfac = 0.7f;
+        public static float moveForward = -moveSpeed;
+        public static float moveBackward = moveSpeed;
+
+    }
+}
---
+++ Vampir/Vampir/Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Vampir
+{
+    class Item
+    {
+        public Vector2f _position;
+        Sprite _sprite;
+        public float width, height;
+
+        public Item(string path, float X, float Y)
+        {
+            _position = new Vector2f(X, Y);
+            Texture tex = new Texture(path);
+            _sprite = new Sprite(tex);
+
+            width = tex.Size.X;
+            height = tex.Size.Y;
+        }
+
+        public void Update(Vector2f vec)
+        {
+            _position.X += vec.X;
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            _sprite.Position = _position;
+            window.Draw(_sprite);
+        }
+    }
+}
