53e0d55c2402f5698eb727de0e41cbe3fd056da0
Haschiwu <christoph.hollmann@yahoo.de>
2014-07-28 14:55:46 +0200

    awfdwa

---
+++ Vampir/Vampir/Creature.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+using Vampir;
+
+namespace Vampir
+{
+    class Creature
+    {
+        public Vector2f position;
+        public float width, height;
+        public Sprite sprite;
+
+        public Creature()
+        {
+            position = new Vector2f(0, 0);
+            width = 0; height = 0;
+            sprite = new Sprite();
+        }
+    }
+}
