b2325d65ac3f69cc4c4e2265d1c7fa9327d14034
Haschiwu <christoph.hollmann@yahoo.de>
2014-07-29 20:26:09 +0200

    level 2

--- Vampir/Vampir/Levels.cs
+++ Vampir/Vampir/Levels.cs
@@ -34,9 +34,10 @@ namespace Vampir
                         for (int i = 0; i < 2; i++)
                             map.loadContent(new Vampir.background("Graphiken/hintergrund.png"));
                         list = new List<Thing>();
+
                         mList = new List<Thing>();
                         mList.Add(new Werwolf(monster_tex, 1500, 0));
-                        float[,] items = new float[,] {{1000,0},{1100,0},{1080,100},{680,200},
+                        float[,] items = new float[,] {{1000,0},{1100,0},{1080,100},
                         {2000,0},{2000,100},{2000,200},{1500,200},{2100,100},{2200,0}};
 
                         for (int i = 0; i < items.GetLength(0); i++)
@@ -56,12 +57,24 @@ namespace Vampir
                             map.loadContent(new Vampir.background("Graphiken/hintergrund.png"));
                         list = new List<Thing>();
                         mList = new List<Thing>();
-                        mList.Add(new Werwolf(monster_tex, 1500, 0));
-                        mList.Add(new Werwolf(monster_tex, 2400, 0));
-                        mList.Add(new Werwolf(monster_tex, 3300, 0));
-                        mList.Add(new Werwolf(monster_tex, 5100, 0));
 
-                        list.Add(new Item(item_tex, 100, 0));
+                        float[,] items = new float[,] {{650,0},{690,100},{890,100},{1090,100},
+                        {2000,0},{2000,100},{2000,200},{1500,200},{2100,100},{2200,0},
+                        {2500,800}, {2600,150},{2800,150},{3000,150},{3200,310},{3800,0},{3900,100},{4000,200},{4100,300},
+                        {4300,300}, {4650,500}, {4650,400}, {4650,300},{4650,200},{5800,0},{5800,49},{5801,100}};
+
+                        float[,] monster = new float[,] {{1100,200},{1400,0},{2650,950},{3200,410},{3300,0},
+                        {4540,0},{4605,0},{5100,0}};
+
+                        for (int i = 0; i < items.GetLength(0); i++)
+                        {
+                            list.Add(new Item(item_tex, items[i, 0], items[i, 1]));
+                        }
+
+                        for (int i = 0; i < monster.GetLength(0); i++)
+                        {
+                            mList.Add(new Werwolf(monster_tex, monster[i, 0], monster[i, 1]));
+                        }
                         levels[1] = new Level(player, map, list, mList, "Graphiken/Level1.png");
                     }
                     break;
